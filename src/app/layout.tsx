e3bf25e331d5769991348c282f9da1e31bf2a587
rootvishal <v707jadhav@gmail.com>
2026-02-12 07:34:57 +0530

    adding google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-TKQ4LJSVH9"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-TKQ4LJSVH9');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
